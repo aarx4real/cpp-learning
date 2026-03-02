155d774319eae35fb5e5ac5732fbe19a7c7c8cb3
Aarush <aarushgupt29@gmail.com>
2026-03-02 21:35:00 +0530

    learnt about constructors

--- Oop/constructor.cpp
+++ Oop/constructor.cpp
@@ -9,7 +9,9 @@ class Student{
     Student(std::string name, int age, double gpa){     // Constructor - Special method that is automaitcally called when
             this->name = name;                          // an object is instantiated useful for assaiging values to an attributes 
             this->age=age;                              // as arguments
-            this->gpa=gpa;                              
+            this->gpa=gpa;
+            //this-> is a special pointer in c++ that points to the current object
+            //this->property_name=parameter                           
         }                                               
 
         void display(){
