61a3de7c2bebe99930024c9d79f3e9944da6c0f7
Aarush <aarushgupt29@gmail.com>
2026-04-28 19:22:38 +0530

    Function template

--- Templates/templates.cpp
+++ Templates/templates.cpp
@@ -2,10 +2,19 @@
 using namespace std;
 
 template <typename T>
-T Max(T a, T b){
+T Max1(T a, T b){                       //only accepts arguments of a single type
     return(a>b) ? a : b ;
 }
+
+template <typename T, typename U>
+auto Max2(T x, U y){                    //can accept arguments of two differnet types
+    return(x>y) ? x : y; 
+}
+
 int main(){
-    cout << Max(2,4) << endl;
+    cout << Max1(2,4) << endl;
+    cout << Max2(2,4) << endl;
+    cout << Max2(2.4,4.2) << endl;
+    cout << Max2('A',67) << endl;
     return 0;
 }
